46f834f142cc0ac625d80e725114f3b09c51579d
Zhouwy <zwysun@outlook.com>
2021-05-23 11:29:44 +0800

    Update and rename daka.py to hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests, json, re
-import time, datetime, os, sys
+import time, datetime, os
 import getpass
 
 # 环境变量
@@ -12,10 +12,6 @@ password = os.environ["PASSWORD"]
 # password = ''
 # DingTalk的sckey
 sckey = os.environ["PUSH_KEY"]
-# sckey = ''
-# customize address
-# todo
-
 def send_message(title='无效', text=''):
     if text == '':
         requests.post('https://api.zwya.ga/dingtalk/send?token=' + sckey + '&title=健康打卡通知&text=健康打卡通知 \n\n' + title)
@@ -23,30 +19,34 @@ def send_message(title='无效', text=''):
         requests.post('https://api.zwya.ga/dingtalk/send?card=1&token=' + sckey + '&title=' + title + '&text=' + text)
     return
 
-class DaKa(object):
-    """Hit card class
-
+class HitCarder(object):
+    """Hit carder class
     Attributes:
         username: (str) 浙大统一认证平台用户名（一般为学号）
         password: (str) 浙大统一认证平台密码
         login_url: (str) 登录url
         base_url: (str) 打卡首页url
         save_url: (str) 提交打卡url
+        self.headers: (dir) 请求头
         sess: (requests.Session) 统一的session
     """
-    def __init__(self):
+
+    def __init__(self, username, password):
         self.username = username
         self.password = password
         self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
         self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
         self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+        }
         self.sess = requests.Session()
 
     def login(self):
-        """Login to ZJU platform"""
-        res = self.sess.get(self.login_url)
+        """Login to ZJU platform."""
+        res = self.sess.get(self.login_url, headers=self.headers)
         execution = re.search('name="execution" value="(.*?)"', res.text).group(1)
-        res = self.sess.get(url='https://zjuam.zju.edu.cn/cas/v2/getPubKey').json()
+        res = self.sess.get(url='https://zjuam.zju.edu.cn/cas/v2/getPubKey', headers=self.headers).json()
         n, e = res['modulus'], res['exponent']
         encrypt_password = self._rsa_encrypt(self.password, e, n)
 
@@ -56,27 +56,27 @@ class DaKa(object):
             'execution': execution,
             '_eventId': 'submit'
         }
-        res = self.sess.post(url=self.login_url, data=data)
+        res = self.sess.post(url=self.login_url, data=data, headers=self.headers)
 
         # check if login successfully
         if '统一身份认证' in res.content.decode():
             raise LoginError('登录失败，请核实账号密码重新登录')
         return self.sess
-    
+
     def post(self):
-        """Post the hitcard info"""
-        res = self.sess.post(self.save_url, data=self.info)
+        """Post the hit card info."""
+        res = self.sess.post(self.save_url, data=self.info, headers=self.headers)
         return json.loads(res.text)
-    
+
     def get_date(self):
-        """Get current date"""
+        """Get current date."""
         today = datetime.date.today()
-        return "%4d%02d%02d" %(today.year, today.month, today.day)
-        
+        return "%4d%02d%02d" % (today.year, today.month, today.day)
+
     def get_info(self, html=None):
-        """Get hitcard info, which is the old info with updated new time."""
+        """Get hit card info, which is the old info with updated new time."""
         if not html:
-            res = self.sess.get(self.base_url)
+            res = self.sess.get(self.base_url, headers=self.headers)
             html = res.content.decode()
 
         try:
@@ -91,9 +91,9 @@ class DaKa(object):
             name = re.findall(r'realname: "([^\"]+)",', html)[0]
             number = re.findall(r"number: '([^\']+)',", html)[0]
         except IndexError as err:
-            raise RegexMatchError('Relative info not found in html with regex')
+            raise RegexMatchError('Relative info not found in html with regex: ' + str(err))
         except json.decoder.JSONDecodeError as err:
-            raise DecodeError('JSON decode error')
+            raise DecodeError('JSON decode error: ' + str(err))
 
         new_info = old_info.copy()
         new_info['id'] = new_id
@@ -109,23 +109,22 @@ class DaKa(object):
         # form change
         new_info['jrdqtlqk[]'] = 0
         new_info['jrdqjcqk[]'] = 0
-        new_info['sfsqhzjkk'] = 1   # 是否申领杭州健康码
-        new_info['sqhzjkkys'] = 1   # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
-        new_info['sfqrxxss'] = 1    # 是否确认信息属实
+        new_info['sfsqhzjkk'] = 1  # 是否申领杭州健康码
+        new_info['sqhzjkkys'] = 1  # 杭州健康吗颜色，1:绿色 2:红色 3:黄色
+        new_info['sfqrxxss'] = 1  # 是否确认信息属实
         new_info['sfymqjczrj'] = 0  # 是否密切接触家人入境
         new_info['jcqzrq'] = ""
         new_info['gwszdd'] = ""
         new_info['szgjcs'] = ""
         self.info = new_info
-        # print(old_info, self.info)
         return new_info
 
     def _rsa_encrypt(self, password_str, e_str, M_str):
-        password_bytes = bytes(password_str, 'ascii') 
+        password_bytes = bytes(password_str, 'ascii')
         password_int = int.from_bytes(password_bytes, 'big')
-        e_int = int(e_str, 16) 
-        M_int = int(M_str, 16) 
-        result_int = pow(password_int, e_int, M_int) 
+        e_int = int(e_str, 16)
+        M_int = int(M_str, 16)
+        result_int = pow(password_int, e_int, M_int)
         return hex(result_int)[2:].rjust(128, '0')
 
 
@@ -134,52 +133,65 @@ class LoginError(Exception):
     """Login Exception"""
     pass
 
+
 class RegexMatchError(Exception):
     """Regex Matching Exception"""
     pass
+
 
 class DecodeError(Exception):
     """JSON Decode Exception"""
     pass
 
 
-def main():
+def main(username, password):
     """Hit card process
-
+    Arguments:
+        username: (str) 浙大统一认证平台用户名（一般为学号）
+        password: (str) 浙大统一认证平台密码
     """
     start_time = ("\n[Time] %s" %datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-    # print("🚌 打卡任务启动")
+    print("\n[Time] %s" % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print("🚌 打卡任务启动")
 
-    dk = DaKa()
+    hit_carder = HitCarder(username, password)
 
+
+    print('登录到浙大统一身份认证平台...')
     try:
-        dk.login()
+        hit_carder.login()
+        print('已登录到浙大统一身份认证平台')
     except Exception as err:
-        # spinner.fail(str(err))
+        print(str(err))
         return
 
-    # spinner.start(text='正在获取个人信息...')
+    print('正在获取个人信息...')
     try:
-        dk.get_info()
-        personal_info = ('%s %s同学, 你好~' %(dk.info['number'], dk.info['name']))
+        hit_carder.get_info()
+        personal_info = ('%s %s同学, 你好~' % (hit_carder.info['number'], hit_carder.info['name']))
+        print('%s %s同学, 你好~' % (hit_carder.info['number'], hit_carder.info['name']))
     except Exception as err:
+        print('获取信息失败，请手动打卡，更多信息: ' + str(err))
         send_message(title='获取信息失败，请手动打卡，更多信息:', text=str(err))
         return
 
-    # spinner.start(text='正在为您打卡')
+    print('正在为您打卡...')
     try:
-        res = dk.post()
+        res = hit_carder.post()
         if str(res['e']) == '0':
+            print('已为您打卡成功！')
             send_message(title='打卡🎈成功!', text=start_time+'\n\n'+personal_info+'\n\n From HealthCheck.')
         else:
+            print(res['m'])
             send_message(title=res['m']+'[Check here](https://healthreport.zju.edu.cn/ncov/wap/default/index)')
-    except:
-        send_message(title='数据提交失败')
-        return 
+    except Exception as err:
+        print('数据提交失败 ' + str(err))
+        send_message(title='数据提交失败' + str(err))
+        return
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
-        main()
+        main(username, password)
     except (KeyboardInterrupt, SystemExit):
         pass
